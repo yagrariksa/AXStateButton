e91b6e0225c588d8ecd272b8805e4d4a28e4dd9b
yagrariksa <luckyrama0@gmail.com>
2024-02-07 11:49:51 +0700

    add SPM Availability
    Signed-off-by: yagrariksa <luckyrama0@gmail.com>

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "AXStateButton",
+    platforms: [
+        .iOS("8.0")
+    ],
+    products: [
+        .library(
+            name: "AXStateButton",
+            targets: ["AXStateButton"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "AXStateButton",
+            dependencies: [],
+            path: "Source"
+        )
+    ]
+)
